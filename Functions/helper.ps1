c55c187cf707567b36c9c056950626e059605011
Bashir Momen <bashir.momen@amcsgroup.com>
2020-02-28 10:55:40 +0100

    Added startup hint

--- Functions/helper.ps1
+++ Functions/helper.ps1
@@ -1,34 +1,61 @@
-function repos {
-     Write-Host ""
-     Write-Host "[I]  Identity Service" -f Green
-     Write-Host "[IU] Identity Service UI" -f Green
-     Write-Host "[T]  Transport and Dispatch" -f Green
-     Write-Host "[TU] Transport and Dispatch UI" -f Green
-     Write-Host "[P]  Platform" -f Green
-     Write-Host "[PU] Platform UI" -f Green
-     Write-Host "[WP] Windows PowerShell Profile" -f Green
+function ChangeToOtherRepos($repo) {
      
-    $input = Read-Host "Please Choose your repo"
+     if($repo -notmatch "\S"){
+
+        Write-Host ""
+        Write-Host "[I]  Identity Service" -f Green
+        Write-Host "[IU] Identity Service UI" -f Green
+        Write-Host "[T]  Transport and Dispatch" -f Green
+        Write-Host "[TU] Transport and Dispatch UI" -f Green
+        Write-Host "[P]  Platform" -f Green
+        Write-Host "[PU] Platform UI" -f Green
+        Write-Host "[WP] Windows PowerShell Profile" -f Green
+     
+        $repo = Read-Host "Please Choose your repo"
+    }
 
     $mainPath ="$home\source\repos\AMCS\"
 
-    if ($input -eq 'I') {
+    if ($repo -eq 'I') {
         Set-Location "$mainPath\IdentityService\"
-    }elseif ($input -eq 'IU'){
+    }elseif ($repo -eq 'IU'){
         Set-Location "$mainPath\IdentityServiceUI\"
-    }elseif ($input -eq 'T'){
+    }elseif ($repo -eq 'T'){
         Set-Location "$mainPath\TransportDispatch\"
-    }elseif ($input -eq 'TU'){
+    }elseif ($repo -eq 'TU'){
         Set-Location "$mainPath\TransportDispatchUI\"
-    }elseif ($input -eq 'P'){
+    }elseif ($repo -eq 'P'){
         Set-Location "$mainPath\Platform\"
-    }elseif ($input -eq 'PU'){
+    }elseif ($repo -eq 'PU'){
         Set-Location "$mainPath\PlatformUI\"
-    }elseif ($input -eq 'wp'){
+    }elseif ($repo -eq 'wp'){
         Set-Location "$home\Documents\WindowsPowershell\"
+    }else{
+        Write-Host "Repo not found" -f red
     }
 
 
     Write-Host ""
 }
 
+
+function startupHint {
+    Write-Host "AMCS Useful Development Commands:" -f Yellow
+    Write-Host ""
+    Write-Host "(CMD) Command Explanation       Params <required> [optional]" -f Yellow
+    Write-Host "----- ------------------------- ---------------------------- " -f Yellow
+    Write-Host "(REP) Change dir to other repos [repo short name]" -f cyan
+    Write-Host "" 
+    Write-Host "How to run:" -f cyan
+    Write-Host "AMCS <CMD> <[Params]>" -f cyan
+    Write-Host "" 
+}
+
+function amcs($command, $par1){
+
+    if($command -eq "REP"){
+        ChangeToOtherRepos $par1
+    }else{
+        Write-Host "Bad command or file name!"
+    }
+}
--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 Resolve-Path $here\Functions\*.ps1 | ForEach-Object { . $_.ProviderPath }
 
+startupHint
